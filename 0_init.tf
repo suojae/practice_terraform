c8fad1a42dea9f6f263ac0619ab8f9171f18bb16
suojae <suojae.developer@gmail.com>
2024-11-15 12:24:13 +0900

    feat: 키페어 추가

--- 0_init.tf
+++ 0_init.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
@@ -11,3 +11,11 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
+resource "aws_key_pair" "ssuojae_key" {
+  key_name = "ssuojae-key"
+  public_key = file("/Users/jayoengchoi/WebstormProjects/practice_terraform/id_rsa.pub")  # 공개 키 경로
+
+  tags = {
+    Name = "ssuojae-key"
+  }
+}
